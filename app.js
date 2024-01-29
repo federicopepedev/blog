3e99701edab153d644a47d2e42f133ea3945d5c0
federicopepedev <federicopepedev@gmail.com>
2024-01-29 17:30:15 +0100

    built in bp

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
@@ -27,7 +26,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
